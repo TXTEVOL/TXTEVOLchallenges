016771a7f92ad6c156641b19078e488133971045
TXTEVOL <allanholland12@gmail.com>
2017-07-26 16:38:44 -0700

    Add files via upload

---
+++ home/home.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SearchPage } from '../search/search';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+goToSearch(){
+   this.navCtrl.push(SearchPage);
+  }
+}
---
+++ search/search.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { ProgramPage } from '../program/program';
+
+/**
+ * Generated class for the SearchPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html',
+})
+export class SearchPage {
+
+searchQuery: string = '';
+  items: string[];
+ 
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  this.initializeItems();
+  }
+ 
+initializeItems() {
+    this.items = [
+      'Basketball Camps- IMG Academy 2017',
+      'Nike Basketball Camps- NIKE Sports Camps',
+    ];
+  }
+  
+  getItems(ev:any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+   
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SearchPage');
+  }
+goToProgram(){
+    this.navCtrl.push(ProgramPage);
+  }
+
+}
